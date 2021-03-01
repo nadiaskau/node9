4a6294c592ae958a8318675805d74b2b79d83e0f
nadiaskau <nadiaskau@hotmail.com>
2021-03-01 11:38:26 +0100

    Initial solution

---
+++ world/models/handleCountries.js
@@ -0,0 +1,59 @@
+"use strict";
+const mon = require("./mongoWrap");
+const dbServer = "localhost";
+const dbName = "world";
+
+exports.getCountries = async function (res) {
+    try {
+        let cs = await mon.retrieve(dbServer, dbName, "country", {});
+        res.render('country', {
+            title: 'Fragments of the World',
+            subtitle: 'Select Country',
+            countries: cs
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+exports.getCountry = async function (res, ctryname) {
+    try {
+        let cs = await mon.retrieve(dbServer, dbName, "country", { "name": ctryname });
+        res.render('countryDisplay', {
+            title: 'Fragments of the World',
+            subtitle: ctryname,
+            countries: cs
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+exports.postCountry = async function (req, res, next) {
+    let chk = { name: req.body.name };  // check object for existence
+    let country = {                     // create obejct in db-format
+        code: req.body.code,
+        name: req.body.name,
+        continent: req.body.continent,
+        region: req.body.region,
+        surfacearea: req.body.surfacearea,
+        indepyear: req.body.indepyear,
+        population: req.body.population,
+        lifeexpectancy: req.body.lifeexpectancy,
+        gnp: req.body.gnp,
+        gnpold: req.body.gnpold,
+        localname: req.body.localname,
+        governmentform: req.body.governmentform,
+        headofstate: req.body.headofstate,
+        capital: null,
+        code2: req.body.code2
+    };
+    if (req.body.localname === "") country.localname = country.name;
+    console.log(req.body);
+    try {
+        let cs = await mon.upsert(dbServer, dbName, "country", country, chk);
+        res.redirect("/");
+    } catch (e) {
+        console.log(e);
+    }
+}
---
+++ world/models/mongoWrap.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * wrapper for CRUD functionality of a mongodb
+ */
+const mongo = require('mongodb').MongoClient;
+const conparam = { useNewUrlParser: true, useUnifiedTopology: true};
+
+exports.retrieve = async function(url, dbn, coll, query) {
+    const constr = `mongodb://${url}:27017`;
+    const con = await mongo.connect(constr, conparam);
+    const db = con.db(dbn);
+    let stuff = null;
+    try {
+        stuff = await db.collection(coll).find(query).toArray();
+    } catch(err) {
+        console.log(error);
+    } finally {
+        con.close();
+        return stuff;
+    }
+}
+
+exports.upsert = async function(url, dbn, coll, query, chk) {
+    const constr = `mongodb://${url}:27017`;
+    const con = await mongo.connect(constr, conparam);
+    const db = con.db(dbn);
+    let stuff = null;
+    try {
+        stuff = await db.collection(coll).updateOne(chk, {"$set": query}, {upsert: true});
+    } catch(err) {
+        console.log(error);
+    } finally {
+        con.close();
+        return stuff;
+    }
+}

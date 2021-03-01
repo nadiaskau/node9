19a03e0c5e626458f8bba240ea797e2c5f46a62a
nadiaskau <nadiaskau@hotmail.com>
2021-03-01 12:25:49 +0100

    Added worldMongoose

---
+++ worldMongoose/routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const modCountry = require("../models/handleCountries");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Fragments of the World',
+        subtitle: 'Display and Register World Data'
+    });
+});
+
+router.get('/country', function(req, res, next) {
+    modCountry.getCountries(res);
+});
+router.get('/country/:country', function(req, res, next) {
+    modCountry.getCountry(res, req.params.country);
+});
+router.post("/country", function(req, res, next) {
+    modCountry.getCountry(res, req.body.ctry);
+});
+
+router.get('/countryData', function(req, res, next) {
+    modCountry.getContinentsAndGovernment(res);
+});
+router.post("/countryData", function(req, res, next) {
+    modCountry.postCountry(req, res, next);
+});
+
+module.exports = router;
